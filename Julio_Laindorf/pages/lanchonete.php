131c9870f30d5ae55eb94dd88996d549e204e799
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-29 22:01:18 -0300

    ex 2 ok

--- Julio_Laindorf/pages/lanchonete.php
+++ Julio_Laindorf/pages/lanchonete.php
@@ -3,9 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Lanchonete</title>
 </head>
+<h1>Lanchonete</h1>
 <body>
-  lanchonete
+
+<form method="post" action="calcula_preco.php">
+  <label for="codigo">Digite o código do Produto</label>
+  <input type="number" name="codigo">
+  <label for="quantidade">Digite a quantidade</label>
+  <input type="number" name="quantidade">
+
+  <button type="submit">Solicitar</button>
+
+</form>
+
 </body>
 </html>
