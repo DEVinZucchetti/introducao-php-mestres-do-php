b416fd434271691e69d2993ec1979080f8454d54
0Duarte <guiduartesbs@hotmail.com>
2023-09-30 17:05:04 -0300

    Exercício média ok

--- GuilhermeD/pages/media.php
+++ GuilhermeD/pages/media.php
@@ -6,6 +6,17 @@
   <title>Document</title>
 </head>
 <body>
-  media
+<h1>Calculadora Média</h1>
+  <form method="POST" action="calcular_media.php">
+    <label for="nota1">Nota1</label>
+    <input type="number" id="nota1" name="nota1">
+    <label for="nota2">Nota2</label>
+    <input type="number" id="nota2" name="nota2">
+    <label for="nota3">Nota3</label>
+    <input type="number" id="nota3" name="nota3">
+    <label for="nota4">Nota4</label>
+    <input type="number" id="nota4" name="nota4">
+    <button type="submit">Calcular</button>
+  </form>
 </body>
 </html>
--- home.php
+++ home.php
@@ -54,7 +54,7 @@
     <div class="chat">
 
         <div class="message sent">
-            <p><span class="author">Nome do aluno</span>: Olá,  professor!  <a href="./GuilhermeD/"> Clique aqui a minha solução.</a>.</p>
+            <p><span class="author">Guilherme Duarte</span>: Olá,  professor!  <a href="./GuilhermeD/"> Clique aqui a minha solução.</a>.</p>
            
         </div>
         <!-- Adicione mais mensagens aqui -->
