01e4837b93f7704bc0345bde5faf12025cc1a4d7
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-10-01 13:56:29 -0300

    Feat: Add calculo Lanchonete

--- Julio_Laindorf/pages/calcula_preco.php
+++ Julio_Laindorf/pages/calcula_preco.php
@@ -2,7 +2,43 @@
 
 
 
+$codigo = filter_var($_POST['codigo'], FILTER_VALIDATE_FLOAT);
 
+$quantidade = filter_var($_POST['quantidade'], FILTER_VALIDATE_FLOAT);
 
+if(!$codigo || !$quantidade ){
+    echo "O código e a quantidade são necessários";
+    exit;
+}
+else{
+    switch($codigo) {
+        case 1 : {
+            $valor = $quantidade * 5;
+            echo "Você pediu $quantidade torradas e vai pagar R$" .number_format($valor,2);
+            break;
+        }
+        case 2 : {
+            $valor = $quantidade * 6 ;
+            echo "Você pediu $quantidade torradas e vai pagar R$" .number_format($valor,2);
+            break;
+        }
+        case 3: {
+            $valor = $quantidade * 8;
+            echo "Você pediu $quantidade torradas e vai pagar R$" .number_format($valor,2);
+            break;
+        }
+        case 4: {
+            $valor = $quantidade * 15;
+            echo "Você pediu $quantidade torradas e vai pagar R$" .number_format($valor,2);
+            break;
+        }
+        default: {
+            echo "Código não localizado";
+            break;
+        }
+
+    }
+  
+}
 
 ?>
