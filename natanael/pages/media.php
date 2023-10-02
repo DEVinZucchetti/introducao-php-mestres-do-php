7a5cdf8ada4e5a67433857ee0f197df506b37fe9
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-10-02 18:59:41 -0300

    feat calculo media

--- natanael/pages/media.php
+++ natanael/pages/media.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Media</title>
 </head>
+
 <body>
-  media
+  <form method="post" action="calcular_media.php">
+    <label>nota 1</label>
+    <input type="number" name="nota1"> </br>
+    <label>nota 2</label>
+    <input type="number" name="nota2"></br>
+    <label>nota 3</label>
+    <input type="number" name="nota3"> </br>
+    <label>nota 4</label>
+    <input type="number" name="nota4"></br>
+    <button type="submit">Calcular</button>
+  </form>
 </body>
+
 </html>
