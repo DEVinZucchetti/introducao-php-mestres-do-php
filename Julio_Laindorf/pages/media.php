bf19d4e017cd499c6d4d000de4369004710ddbe9
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-10-02 13:15:15 -0300

    feat: média

--- Julio_Laindorf/pages/media.php
+++ Julio_Laindorf/pages/media.php
@@ -3,9 +3,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Média</title>
 </head>
 <body>
-  media
+<h1>Calcular Média</h1>
+<form method="post" action="calcula_media.php">
+  <label >Digite a nota 1</label>
+  <input type="number" step="any" name="primeira">
+  <label >Digite a nota 2</label>
+  <input type="number" step="any" name="segunda">
+  <label >Digite a nota 3</label>
+  <input type="number" step="any" name="terceira">
+  <label >Digite a nota 4</label>
+  <input type="number" step="any" name="quarta">
+
+  <button type="submit">Calcular</button>
+
+</form>
+
 </body>
 </html>
