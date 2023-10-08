7015114b495a753dae9b81b73e2a6597a95ec411
joseCnarciso <josecdia@hotmail.com>
2023-10-08 20:54:18 -0300

    Codigo que faz o calculo de media de quatro valores informados

--- joseCnarciso/pages/media.php
+++ joseCnarciso/pages/media.php
@@ -3,9 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Média</title>
 </head>
 <body>
-  media
+<form method="post" action="calcular_media.php">
+
+<label>Informe as notas aqui</label>
+</br>
+    <label>nota 1</label>
+    <input type="number" name="nota1"> </br>
+    <label>nota 2</label>
+    <input type="number" name="nota2"></br>
+    <label>nota 3</label>
+    <input type="number" name="nota3"> </br>
+    <label>nota 4</label>
+    <input type="number" name="nota4"></br>
+    <button type="submit">Calcular</button>
+  </form>
 </body>
 </html>
