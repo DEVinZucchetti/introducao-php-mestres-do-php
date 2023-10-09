722daa5d43dbd9712c864566cc9989be029d9761
joseCnarciso <josecdia@hotmail.com>
2023-10-08 21:43:07 -0300

    Codigo que simula uma lanchonete onde deve ser informado o codigo do produto e quantidade que deseja comprar/ possui uma tabela demonstrativa de produtos com codigo/ valor e descricao

--- joseCnarciso/pages/lanchonete.php
+++ joseCnarciso/pages/lanchonete.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -6,6 +7,39 @@
   <title>Document</title>
 </head>
 <body>
-  lanchonete
+  <form method="post" action="lanchonete_pedido.php">
+    <label>Código</label>
+    <input type="number" name="codigo"> </br>
+    <label>Quantidade</label>
+    <input type="number" name="quantidade"></br>
+    <button type="submit">Fazer Pedido</button>
+  </form>
+
+  <table border="1">
+    <thead>
+      <tr>
+        <th>Código</th>
+        <th>Preço</th>
+        <th>Descrição do Produto</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      include('lanchonete_pedido.php');
+
+      if (isset($codigosCadastrados) && !empty($codigosCadastrados)) {
+          foreach ($codigosCadastrados as $codigoCadastrado => $info) {
+              echo "<tr>";
+              echo "<td>$codigoCadastrado</td>";
+              echo "<td>{$info['preco']}</td>";
+              echo "<td>{$info['descricao']}</td>";
+              echo "</tr>";
+          }
+      } else {
+          echo "<tr><td colspan='3'>Nenhum código cadastrado</td></tr>";
+      }
+      ?>
+    </tbody>
+  </table>
 </body>
 </html>
