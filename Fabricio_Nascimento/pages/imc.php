b8603627e002d64d9e99ff35f70ab6f5e1333c38
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-11 11:40:46 +0100

    inserindo formulario

--- Fabricio_Nascimento/pages/imc.php
+++ Fabricio_Nascimento/pages/imc.php
@@ -3,9 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Calculadora IMC</title>
 </head>
 <body>
-  imc
+  <form method="post" action="calcular_imc.php">
+    <label>Peso</label>
+    <br>
+    <input type="text" inputmode="numeric" name="weight">
+    <br>
+    <label>Altura</label>
+    <br>
+    <input type="text" inputmode="numeric" name="height">
+    <br>
+    <button type="submit">Calcular</button>
+  </form>
 </body>
 </html>
