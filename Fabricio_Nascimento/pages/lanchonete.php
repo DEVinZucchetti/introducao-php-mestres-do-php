e39711391962af5731eee605fc475ce49e170b99
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-11 12:21:28 +0100

    inserindo form

--- Fabricio_Nascimento/pages/lanchonete.php
+++ Fabricio_Nascimento/pages/lanchonete.php
@@ -3,9 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Lanchonete</title>
 </head>
 <body>
-  lanchonete
+
+<h2>INFORME SEU PEDIDO COM OS SEGUINTES CODIGOS: </h2>
+
+<p>100 - REFRIGERANTE | 101 - SANDUICHE | 102 - SOPA | 103 - SUCO | 104 - SALADA | 105 - VITAMINA</p>
+<br>
+
+
+<form method="post" action="calcular_preco.php">
+    <label>Código</label>
+    <br>
+    <input type="number"  name="codigo">
+    <br>
+    <label>Quantidade</label>
+    <br>
+    <input type="text" inputmode="numeric" name="quantidade">
+    <br>
+    <button type="submit">Calcular valor</button>
+  </form>
 </body>
 </html>
